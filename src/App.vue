3b47776848854cab449b598ab70051c884abec8c
BrianL <BrianL@yeah.net>
2021-02-25 11:11:59 +0800

    init gshop

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <FooterGuide v-show="$route.meta.showFooter"/>
+  </div>
+</template>
+
+<script>
+  import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+  import ajax from './api/ajax.js'
+  var ajax1 = ajax("aaa","",).data();
+
+
+  export default {
+    components:{
+      FooterGuide
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .app
+    width 400%
+    height 100%
+    background #f5f5f5
+    color: red
+    position relative
+</style>
